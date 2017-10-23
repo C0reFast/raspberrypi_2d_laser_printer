0ae7b50988a4c8583ff262d4070bd0312dd4bc8c
chenfu <chenfu@staff.sina.com.cn>
2017-10-23 16:24:07 +0800

    完善axis控制代码

--- printer.py
+++ printer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import time
 import RPi.GPIO as GPIO
 
 
@@ -19,19 +20,46 @@ class Axis(object):
         self._io_dir = io_dir
         self._io_step = io_step
         self._current_pos = 0
+        self._pulse_width_us = 2
+        self._max_steps = max_steps
+
         GPIO.setup(self._io_dir, GPIO.OUT)
-        GPIO.output(self._io_dir, 0)
+        GPIO.output(self._io_dir, GPIO.LOW)
         GPIO.setup(self._io_step, GPIO.OUT)
-        GPIO.output(self._io_step, 0)
+        GPIO.output(self._io_step, GPIO.HIGH)
+
+    def step(self, direction):
+        """One Step for direction
+
+        :direction: direction
+
+        """
+        GPIO.output(self._io_dir, GPIO.LOW if direction else GPIO.HIGH)
+        GPIO.output(self._io_step, GPIO.HIGH)
+        time.sleep(self._pulse_width_us / float(1000000))
+        GPIO.output(self._io_step, GPIO.LOW)
 
     def move_to(self, pos):
         """Move To Specific Position
 
         :pos: Specific Position
         """
-        pass
+        if pos > self._max_steps:
+            pos = self._max_steps
+        if pos < 0:
+            pos = 0
+        if pos < self._current_pos:
+            steps = self._current_pos - pos
+            direction = True
+        else:
+            steps = pos - self._current_pos
+            direction = False
+        for _ in range(steps):
+            self.step(direction)
+        self._current_pos = pos
 
     def reset(self):
-        """Reset to the Zere Position
+        """Reset to the Zero Position
         """
-        pass
+        for _ in range(self._current_pos+20):
+            self.step(False)
